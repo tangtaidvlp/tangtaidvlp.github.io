9d9d1f8e42d00e8371c1e54756a1e51f6ffbe821
TaiTang <tangtaidvlp@gmail.com>
2021-12-01 23:30:56 +0700

    Add footer

---
+++ js/index_code.js
@@ -0,0 +1,13 @@
+var target = document.querySelector('#about-me-section');
+var nav_project = document.querySelector('#nav-project');
+var nav_about = document.querySelector('#nav-about');
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > target.getBoundingClientRect().top) {
+    nav_project.className = "nav-link";
+    nav_about.className = "nav-link active";
+  } else {
+    nav_project.className = "nav-link active";
+    nav_about.className = "nav-link";
+  }
+})
