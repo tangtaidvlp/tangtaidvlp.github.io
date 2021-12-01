8c1e5c7a50d997ef048dca41cd9d28167b678a31
TaiTang <tangtaidvlp@gmail.com>
2021-12-01 22:10:16 +0700

    Almost finish Tom effect

--- js/tom_code.js
+++ js/tom_code.js
@@ -1,3 +1,16 @@
 function onImageHover (imageDiv) {
-    
+    var tomImages = imageDiv.parentNode.getElementsByClassName("tom-image --js");
+    for (var tomImage of tomImages) {
+        if (tomImage != imageDiv) {
+            tomImage.style.opacity = "0.2";
+            console.log(tomImage);
+        } 
+    }
+}
+
+function onImageOut(imageDiv) {
+    var tomImages = imageDiv.parentNode.getElementsByClassName("tom-image --js");
+    for (var tomImage of tomImages) {
+        tomImage.style.opacity = "1.0";
+    }
 }
