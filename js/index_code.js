ad40bb08226809bad420624c7de27138a768c219
TaiTang <tangtaidvlp@gmail.com>
2021-12-02 18:08:27 +0700

    Complete Index.html

--- js/index_code.js
+++ js/index_code.js
@@ -1,13 +1,18 @@
+var navbar = document.getElementsByClassName('nav-bar')[0];
+
 var target = document.querySelector('#about-me-section');
 var nav_project = document.querySelector('#nav-project');
 var nav_about = document.querySelector('#nav-about');
 
 document.addEventListener('scroll', () => {
+
   if (window.scrollY > target.getBoundingClientRect().top) {
     nav_project.className = "nav-link";
     nav_about.className = "nav-link active";
   } else {
     nav_project.className = "nav-link active";
     nav_about.className = "nav-link";
-  }
+  } 
+
+
 })
