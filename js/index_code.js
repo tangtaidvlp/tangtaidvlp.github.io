ce8965ddc814e48e1ef92ca7923ef6cbb380264b
TaiTang <tangtaidvlp@gmail.com>
2021-12-03 15:32:13 +0700

    Add link thumbnail

--- js/index_code.js
+++ js/index_code.js
@@ -1,17 +1,18 @@
 var navbar = document.getElementsByClassName('nav-bar')[0];
 
-var target = document.querySelector('#about-me-section');
-var nav_project = document.querySelector('#nav-project');
-var nav_about = document.querySelector('#nav-about');
+var $target = $('#about-me-section');
+var $nav_project = $('#nav-project');
+var $nav_about = $('#nav-about');
 
 document.addEventListener('scroll', () => {
+  console.log("window.scrollY = " + window.scrollY);
+  console.log("** " + $target.position().top);
 
-  if (window.scrollY > target.getBoundingClientRect().top) {
-    nav_project.className = "nav-link";
-    nav_about.className = "nav-link active";
+  if (window.scrollY > $target.position().top) {
+    $nav_project.removeClass("active");
+    $nav_about.addClass("active");
   } else {
-    nav_project.className = "nav-link active";
-    nav_about.className = "nav-link";
+    $nav_project.addClass("active");
+    $nav_about.removeClass("active");
   } 
-
 })
